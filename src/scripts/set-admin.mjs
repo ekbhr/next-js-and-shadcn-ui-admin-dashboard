468fe94b553ff281110ff3351318d228bc5ea60d
Shah Faisal <shahfaisal106@gmail.com>
2025-12-10 13:50:27 +0400

    Remove temporary set-role endpoint and scripts

--- src/scripts/set-admin.mjs
+++
@@ -1,8 +0,0 @@
-/**
- * Script to set a user as admin
- * Run with: npx prisma db execute --file src/scripts/set-admin.sql
- */
-
-// This is just a placeholder - we'll use Prisma CLI instead
-console.log("Use: npx prisma db execute --stdin");
-
--- src/scripts/set-admin.sql
+++
@@ -1,3 +0,0 @@
--- Set shahfaisal106@gmail.com as admin
-UPDATE "User" SET role = 'admin' WHERE email = 'shahfaisal106@gmail.com';
-
